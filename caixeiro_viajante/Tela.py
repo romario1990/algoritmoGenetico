3be2c90abc65f9251c30616fa03b8a312487bd60
romario1990 <romario.getulio@gmail.com>
2018-12-10 13:53:53 -0200

    Adicionatado tela no projeto

--- caixeiro_viajante/Tela.py
+++ caixeiro_viajante/Tela.py
@@ -4,7 +4,6 @@
 import functools
 import tkinter as t
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 from GerenciadorRota import GerenciadorRota
 from Cidade import Cidade
 from Populacao import Populacao
@@ -17,7 +16,10 @@ root.resizable(0, 0)
 f = plt.figure(figsize=(10, 5))
 a = f.add_subplot(111)
 plt.axis('off')
+
 gerenciadorRota = GerenciadorRota()
+pop = None
+ag = None
 cidades = []
 index = 0
 tPopulacao = 100
@@ -25,6 +27,7 @@ tMutacao = 0.5
 tCruzamento = 60
 iteracoes = 50
 mostrar = 5
+tGeracoes = 10
 
 def mostrarPlanoCartesiano(distancia, nome):
     x = []
@@ -91,8 +94,8 @@ class InserirCidade(t.Frame):
 
     def salvarVertice(self):
         nome = self.cidade.get()
-        x = self.eixoX.get()
-        y = self.eixoY.get()
+        x = int(self.eixoX.get())
+        y = int(self.eixoY.get())
 
         podeInserirCidade = True
 
@@ -131,12 +134,13 @@ class RemoverCidade(t.Frame):
     def salvarVertice(self):
         nome = self.cidade.get()
         indice = 0
-
+        global index
         for cid in cidades:
             if cid.getNome() == nome:
                 print(" " + nome + ". Removendo cidade")
                 cidades.remove(cid)
                 gerenciadorRota.removerCidade(cid)
+                index = index - 1
 
 
 
@@ -153,22 +157,27 @@ class CarregarInformacoes(t.Frame):
         t.Label(self, text="Carregar informções").pack()
         self._tela = "carregarinformacoes"
 
-        t.Label(self, text="Tamanho da população").pack()
+        t.Label(self, text="Tamanho da população mínimo de 100 indivíduos").pack()
         self.tamanhoPopulacao = t.Entry(self)
         self.tamanhoPopulacao.bind("<Return>", self.salvarVertice)
         self.tamanhoPopulacao.pack()
 
-        t.Label(self, text="Taxa Mutação").pack()
+        t.Label(self, text="Taxa Mutação faixa de 0.5% - 1%.").pack()
         self.taxaMutacao = t.Entry(self)
         self.taxaMutacao.bind("<Return>", self.salvarVertice)
         self.taxaMutacao.pack()
 
-        t.Label(self, text="Taxa Cruzamento").pack()
+        t.Label(self, text="Taxa Cruzamento faixa de 60%-80%.").pack()
         self.taxaCruzamento = t.Entry(self)
         self.taxaCruzamento.bind("<Return>", self.salvarVertice)
         self.taxaCruzamento.pack()
 
-        t.Label(self, text="Quantidade de iterações").pack()
+        t.Label(self, text="Nº de gerações – mínimo de 10 gerações.").pack()
+        self.nGeracoes = t.Entry(self)
+        self.nGeracoes.bind("<Return>", self.salvarVertice)
+        self.nGeracoes.pack()
+
+        t.Label(self, text="Quantidade de iterações aconselhado no mínimo 100").pack()
         self.iteracoes = t.Entry(self)
         self.iteracoes.bind("<Return>", self.salvarVertice)
         self.iteracoes.pack()
@@ -189,13 +198,15 @@ class CarregarInformacoes(t.Frame):
         global tCruzamento
         global iteracoes
         global mostrar
+        global tGeracoes
 
-        tPopulacao = self.tamanhoPopulacao.get()
-        tMutacao = self.taxaMutacao.get()
-        tCruzamento = self.taxaCruzamento.get()
-        iteracoes = self.iteracoes.get()
-        mostrar = self.mostrar.get()
-        
+        tPopulacao = int(self.tamanhoPopulacao.get())
+        tMutacao = float(self.taxaMutacao.get())
+        tCruzamento = float(self.taxaCruzamento.get())
+        tGeracoes = int(self.nGeracoes.get())
+        iteracoes = int(self.iteracoes.get())
+        mostrar = int(self.mostrar.get())
+
 
 class ExecutarAlgoritmo(t.Frame):
     def __init__(self, parent):
@@ -205,11 +216,60 @@ class ExecutarAlgoritmo(t.Frame):
         t.Label(self, text="Executando").pack()
         self._tela = "executar"
 
+        t.Button(self, text='Executar', command=self.salvarVertice).pack()
+
     def getTela(self):
         return self._tela
 
     def salvarVertice(self):
-        pass
+        global pop
+        global tPopulacao
+        global tMutacao
+        global tCruzamento
+        global iteracoes
+        global mostrar
+        global tGeracoes
+
+        # Inicializa classe da população
+        pop = Populacao(gerenciadorRota)
+
+        # Insere tamanho da população
+        pop.setTamanhoPopulacao(tPopulacao, True)
+
+        print("Distância inicial: " + str(pop.getMaisApto().getDistancia()))
+        print("Solução inicial: \n" + str(pop.getMaisApto()))
+
+
+        # inicializa classe algoritmo genetico
+        ag = AlgoritmoGenetico(gerenciadorRota)
+
+        # Insere taxa mutação
+        ag.setTaxaMutacao(tMutacao)
+
+        ag.setNumeroGeracoes(tGeracoes)
+
+        # Insere taxa de cruzamento
+        ag.setTaxaCruzamento(tCruzamento)
+
+        # Inicializa população
+        pop = ag.evolucaoDaPopulacao(pop)
+
+        # Mostrar plano inicial
+        mostrarPlanoCartesiano(pop.getMaisApto(), "Plano inicial")
+
+        contador = mostrar
+        for i in range(0, iteracoes):
+            if i == contador:
+                mostrarPlanoCartesiano(pop.getMaisApto(), "Plano em processamento")
+                contador = contador + mostrar
+            pop = ag.evolucaoDaPopulacao(pop)
+
+        mostrarPlanoCartesiano(pop.getMaisApto(), "Plano final")
+        print("Finalizado")
+        print("Distância Final: " + str(pop.getMaisApto().getDistancia()))
+        print("solução:")
+        print(pop.getMaisApto())
+
     
 class Menu(t.Frame):
     def __init__(self, parent, *subtelas):
